6c0bf46111731a85a71b2460a63b005a2b2dda72
sumandeepsahu <sumanddepsahu@gmail.com>
2025-01-31 20:57:34 +0530

    new project

--- script.js
+++
@@ -1,9 +0,0 @@
-<div id="p2">
-<div id="p2img"><img src="Screenshot (609).png" alt=""></div>
-<div id="p2link"></div>
-</div>
-
-<div id="p3">
-<div id="p3img"><img src="Screenshot (610).png" alt=""></div>
-<div id="p3link"></div>
-</div>
